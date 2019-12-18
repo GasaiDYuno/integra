26adb73872afe02e68d4411149aa604d11c71844
GasaiDYuno <GasaiDYuno@users.noreply.github.com>
2019-12-18 16:10:45 +0100

    Modification APP JS

--- App.js
+++ App.js
@@ -1,3 +1,45 @@
-function leverCard() {
-    document.getElementById('quinze').style.top = "45%" ;
+function leverCardQuinze() {
+    document.getElementById('quinze').style.top = "30%" ;
+}
+function leverCardQuatorze(){
+    document.getElementById('quatorze').style.top = "30%" ;
+}
+function leverCardTreize(){
+    document.getElementById('treize').style.top = "30%" ;
+}
+function leverCardDouze(){
+    document.getElementById('douze').style.top = "30%" ;
+}
+function leverCardOnze(){
+    document.getElementById('onze').style.top = "30%" ;
+}
+function leverCardDix(){
+    document.getElementById('dix').style.top = "30%" ;
+}
+function leverCardNeuf(){
+    document.getElementById('neuf').style.top = "30%" ;
+}
+function leverCardHuit(){
+    document.getElementById('huit').style.top = "30%" ;
+}
+function leverCardSept(){
+    document.getElementById('sept').style.top = "30%" ;
+}
+function leverCardSix(){
+    document.getElementById('six').style.top = "30%" ;
+}
+function leverCardCinq(){
+    document.getElementById('cinq').style.top = "30%" ;
+}
+function leverCardQuatre(){
+    document.getElementById('quatre').style.top = "30%" ;
+}
+function leverCardTrois(){
+    document.getElementById('trois').style.top = "30%" ;
+}
+function leverCardDeux(){
+    document.getElementById('deux').style.top = "30%" ;
+}
+function leverCardUn(){
+    document.getElementById('un').style.top = "30%" ;
 }
